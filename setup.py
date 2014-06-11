10e18d53abc339cd0567f4a84de3ba051c2c5b18
Doug Elkin <drelkin86@gmail.com>
2014-06-11 08:13:24 -0400

    restructure

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+import setuptools
+from distutils.core import setup
+
+setup(
+    name = 'phoebe',
+    version = '0.5.0.dev1',
+    install_requires = ['beautifulsoup4'],
+    packages = ['phoebe'],
+    )
+
